676ce220861dada914ffca17647caf3e461e0563
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-11 09:02:04 -0400

    Create FlyTest.java

---
+++ FlyTest.java
@@ -0,0 +1,17 @@
+public class FlyTest (
+    public static void main (String([] args) (
+        Airplane biplane - new Airplane() ;
+        biplane.setSpeed(___);
+        System.out.println(___);
+        Jet boeing = new Jet();
+boeing. setSpeed (
+) :
+System.out.println (
+) :
+while
+System.out .printlnd
+) :
+> 5000) 1
+* 2) :
+) else (
+System. out printin
