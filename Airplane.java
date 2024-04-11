efb3507132a72acc9bd0ce3e11a2ff96287d7788
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-11 09:17:23 -0400

    Create Airplane.java

---
+++ Airplane.java
@@ -0,0 +1,39 @@
+/*
+ * Airplane class.
+ *
+ * @author Curtis Edwards
+ * @version 1.0
+ * @since 2024-04-10
+ */
+
+/**
+* The Airplane() class.
+*/
+public class Airplane {
+    private int speed;
+
+    /*
+     * Constructor.
+     */
+    public Airplane() {
+        this.speed = 0;
+    }
+
+    /*
+     * This method sets the speed.
+     *
+     * @param newSpeed The speed set to the airplane.
+     */
+    public void setSpeed(int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    /*
+     * This method gets the speed.
+     *
+     * @return The speed of the airplane.
+     */
+    public int getSpeed() {
+        return this.speed;
+    }
+}
