e6e520d5a1777b562702a9f450d185e2e5c6171c
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 09:31:09 -0400

    Update Jet.java

--- Jet.java
+++ Jet.java
@@ -1,13 +1,36 @@
+/*
+ * Jet class.
+ *
+ * @author Curtis Edwards
+ * @version 1.0
+ * @since 2024-04-13
+ */
+
+/**
+* The Jet() class.
+*/
 public class Jet extends Airplane {
     private static final int MULTIPLIER = 2;
-  
+
+    /**
+     * Constructs a new Jet object.
+     */
     public Jet() {
         super();
     }
+
+    /**
+     * Sets the speed of the jet.
+     *
+     * @param speed the speed to set
+     */
     public void setSpeed(int speed) {
         super.setSpeed(speed * MULTIPLIER);
     }
 
+    /**
+     * Accelerates the jet.
+     */
     public void accelerate() {
         super.setSpeed(getSpeed() * 2);
     }
